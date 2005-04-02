d3c48cf1b6b7fb5f2a5011944757206f05aadeed
Stefan Walter <stefw@src.gnome.org>
2005-04-02 01:54:07 +0000

    Fix crasher in seahorse_util_add_suffix. Remove 'show_length' option.
        * libseahorse/seahorse-util.c: Fix crasher in seahorse_util_add_suffix.
        * data/seahorse.schemas.in: Remove 'show_length' option.

--- libseahorse/seahorse-util.c
+++ libseahorse/seahorse-util.c
@@ -930,7 +930,7 @@ seahorse_util_add_suffix (gpgme_ctx_t ctx, const gchar *path,
     gchar *t;
 	
 	if (suffix == SEAHORSE_CRYPT_SUFFIX)
-        return eel_gconf_get_boolean (ARMOR_KEY) ? SEAHORSE_EXT_ASC : SEAHORSE_EXT_PGP;
+        ext = eel_gconf_get_boolean (ARMOR_KEY) ? SEAHORSE_EXT_ASC : SEAHORSE_EXT_PGP;
 	if (suffix == SEAHORSE_ASC_SUFFIX)
 		ext = SEAHORSE_EXT_ASC;
 	else
