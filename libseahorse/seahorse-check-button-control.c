9f61b31d2e07e8694d6137571b79cdf60e879621
Stefan Walter <stefw@src.gnome.org>
2005-04-16 15:59:35 +0000

    Add proper license.

--- libseahorse/seahorse-check-button-control.c
+++ libseahorse/seahorse-check-button-control.c
@@ -1,3 +1,23 @@
+/*
+ * Seahorse
+ *
+ * Copyright (C) 2003 Jacob Perkins
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #include <eel/eel.h>
 
@@ -78,7 +98,7 @@ seahorse_check_button_control_finalize (GObject *gobject)
 	
 	control = SEAHORSE_CHECK_BUTTON_CONTROL (gobject);
 	
-	eel_gconf_notification_remove (control->notify_id);
+	seahorse_gconf_unnotify (control->notify_id);
 	
 	g_free (control->gconf_key);
 	
