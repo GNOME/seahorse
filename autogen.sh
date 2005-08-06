1c0e8693a1fed3254ba5636e99541e570ee0f396
Stefan Walter <stefw@src.gnome.org>
2005-08-06 20:15:23 +0000

    Require automake 1.7
        * autogen.sh: Require automake 1.7

--- autogen.sh
+++ autogen.sh
@@ -16,12 +16,5 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
     exit 1
 }
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
 
-# This needs a Debian system and the autotools-dev package installed.
-# If you don't have it, please do the following in some other dir:
-# cvs -z 4 -d :pserver:anoncvs@subversions.gnu.org:/cvsroot/config \
-# export -rHEAD config
-# and then copy both config.[guess,sub] files to the seahorse tree before 
-# calling 'make dist' target.
-# cp -f /usr/share/automake-1.6/config.[guess,sub] .
+REQUIRED_AUTOMAKE_VERSION=1.7 USE_GNOME2_MACROS=1 . gnome-autogen.sh
