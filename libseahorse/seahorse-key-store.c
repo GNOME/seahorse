cb61238150d31361fa734b71a00a3b02aa84e692
Stefan Walter <stefw@src.gnome.org>
2005-10-13 19:42:34 +0000

    Don't show key graphic for uids.
        * libseahorse/seahorse-key-store.c: Don't show key graphic for uids.

--- libseahorse/seahorse-key-store.c
+++ libseahorse/seahorse-key-store.c
@@ -379,11 +379,15 @@ seahorse_key_store_set (SeahorseKeyStore *skstore, SeahorseKey *skey,
 {
     gchar *userid = seahorse_key_get_name (skey, uid);
     gboolean sec = seahorse_key_get_etype (skey) == SKEY_PRIVATE;
+    const gchar *stockid = NULL;
+    
+    if (uid == 0)
+        stockid = sec ? SEAHORSE_STOCK_SECRET : SEAHORSE_STOCK_KEY;
     
 	gtk_tree_store_set (GTK_TREE_STORE (skstore), iter,
         KEY_STORE_CHECK, FALSE,
         KEY_STORE_PAIR, uid == 0 ? sec : FALSE,
-        KEY_STORE_STOCK_ID, (uid == 0 && sec) ? SEAHORSE_STOCK_SECRET : SEAHORSE_STOCK_KEY,
+        KEY_STORE_STOCK_ID, stockid,
 		KEY_STORE_NAME, userid,
 		KEY_STORE_KEYID, seahorse_key_get_short_keyid (skey),
         KEY_STORE_UID, uid, -1);
