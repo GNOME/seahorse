9da17b9d0225d379127bb7dd08bf63b83ebdf303
Stefan Walter <stefw@src.gnome.org>
2006-04-16 18:44:32 +0000

    Add license to seahorse-pgp-operation.c
        * libseahorse/seahorse-pgp-operation.c: Add license
        to seahorse-pgp-operation.c

--- libseahorse/seahorse-pgp-operation.c
+++ libseahorse/seahorse-pgp-operation.c
@@ -1,3 +1,23 @@
+/*
+ * Seahorse
+ *
+ * Copyright (C) 2006 Nate Nielsen
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #include "seahorse-pgp-operation.h"
 #include "seahorse-pgp-source.h"
@@ -374,5 +394,3 @@ seahorse_pgp_operation_new (const gchar *message)
 {
     return g_object_new (SEAHORSE_TYPE_PGP_OPERATION, "message", message, NULL);
 }
-
-
