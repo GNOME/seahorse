6034b890f30839cf8343dc6a7c19a9b3a3fe9d1e
Adam Schreiber <sadam@clemson.edu>
2007-10-31 19:47:39 +0000

    Fix incomplete conversion from GtkOptionMenu to GtkComboBox. Fixes bug
    2007-10-31  Adam Schreiber  <sadam@clemson.edu>
    
        * src/seahorse-revoke.c: Fix incomplete conversion from GtkOptionMenu 
        to GtkComboBox.  Fixes bug #475757
    
    svn path=/trunk/; revision=1976

--- src/seahorse-revoke.c
+++ src/seahorse-revoke.c
@@ -29,6 +29,13 @@
 #include "seahorse-libdialogs.h"
 #include "seahorse-util.h"
 
+enum {
+  COLUMN_TEXT,
+  COLUMN_TOOLTIP,
+  COLUMN_INT,
+  N_COLUMNS
+};
+
 static void
 ok_clicked (GtkButton *button, SeahorseWidget *swidget)
 {
@@ -37,13 +44,24 @@ ok_clicked (GtkButton *button, SeahorseWidget *swidget)
 	SeahorseRevokeReason reason;
 	const gchar *description;
 	gpgme_error_t err;
+	GtkWidget *widget;
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+	GValue value;
 	
 	skwidget = SEAHORSE_KEY_WIDGET (swidget);
 	
 	index = skwidget->index;
 	
-	reason = gtk_option_menu_get_history (GTK_OPTION_MENU (
-		glade_xml_get_widget (swidget->xml, "reason")));
+	widget = glade_xml_get_widget (swidget->xml, "reason");
+	model = gtk_combo_box_get_model (GTK_COMBO_BOX (widget));
+	gtk_combo_box_get_active_iter (GTK_COMBO_BOX (widget), &iter);
+	
+    memset(&value, 0, sizeof(value));
+	gtk_tree_model_get_value (model, &iter, COLUMN_INT, &value);
+    reason = g_value_get_int (&value);
+    g_value_unset (&value);
+	
 	description = gtk_entry_get_text (GTK_ENTRY (
 		glade_xml_get_widget (swidget->xml, "description")));
 	
@@ -62,6 +80,10 @@ seahorse_revoke_new (SeahorsePGPKey *pkey, GtkWindow *parent, guint index)
 	SeahorseWidget *swidget;
 	gchar *title;
     gchar *userid;
+    GtkWidget *widget;
+    GtkListStore *store;
+    GtkTreeIter iter;
+	GtkCellRenderer *renderer;
 	
 	g_return_if_fail (pkey != NULL && SEAHORSE_IS_PGP_KEY (pkey));
 	g_return_if_fail (index <= seahorse_pgp_key_get_num_subkeys (pkey));
@@ -81,6 +103,50 @@ seahorse_revoke_new (SeahorsePGPKey *pkey, GtkWindow *parent, guint index)
    
 	gtk_window_set_title (GTK_WINDOW (glade_xml_get_widget (swidget->xml,
 		                  swidget->name)), title);
+
+    /* Initialize List Store for the Combo Box */
+    store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_INT);
+    
+    gtk_list_store_append (store, &iter);
+    gtk_list_store_set (store, &iter,
+                        COLUMN_TEXT, _("No reason"),
+                        COLUMN_TOOLTIP, _("No reason for revoking key"),
+                        COLUMN_INT, REVOKE_NO_REASON,
+                        -1);
+                        
+    gtk_list_store_append (store, &iter);
+    gtk_list_store_set (store, &iter,
+                        COLUMN_TEXT, _("Compromised"),
+                        COLUMN_TOOLTIP, _("Key has been compromised"),
+                        COLUMN_INT, REVOKE_COMPROMISED,
+                        -1);     
+                        gtk_list_store_append (store, &iter);
+
+    gtk_list_store_set (store, &iter,
+                        COLUMN_TEXT, _("Superseded"),
+                        COLUMN_TOOLTIP, _("Key has been superseded"),
+                        COLUMN_INT, REVOKE_SUPERSEDED,
+                        -1);
+
+    gtk_list_store_append (store, &iter);
+    gtk_list_store_set (store, &iter,
+                        COLUMN_TEXT, _("Not Used"),
+                        COLUMN_TOOLTIP, _("Key is no longer used"),
+                        COLUMN_INT, REVOKE_NOT_USED,
+                        -1);
+                        
+    /* Finish Setting Up Combo Box */
+    
+    widget = glade_xml_get_widget (swidget->xml, "reason");
+    
+    gtk_combo_box_set_model (GTK_COMBO_BOX (widget), GTK_TREE_MODEL (store));
+    gtk_combo_box_set_active (GTK_COMBO_BOX (widget), 0);
+    
+    renderer = gtk_cell_renderer_text_new ();
+    gtk_cell_layout_pack_start (GTK_CELL_LAYOUT (widget), renderer, TRUE);
+    gtk_cell_layout_set_attributes (GTK_CELL_LAYOUT (widget), renderer,
+                                    "text", COLUMN_TEXT,
+                                    NULL);
 }
 
 void
