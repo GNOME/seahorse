43a254c17950fcd5423c8518e5e58d7ff95f573e
Adam Schreiber <sadam@gnome.org>
2009-04-29 22:46:16 -0400

    Fix function define formatting

--- libcryptui/cryptui-key-chooser.h
+++ libcryptui/cryptui-key-chooser.h
@@ -83,5 +83,6 @@ const gchar*        cryptui_key_chooser_get_signer          (CryptUIKeyChooser *
 void                cryptui_key_chooser_set_signer          (CryptUIKeyChooser *chooser,
                                                              const gchar *key);
 
-CryptUIKeyChooserMode cryptui_key_chooser_get_mode          (CryptUIKeyChooser *chooser);
+CryptUIKeyChooserMode 
+                    cryptui_key_chooser_get_mode            (CryptUIKeyChooser *chooser);
 #endif /* __CRYPTUI_KEY_CHOOSER_H__ */
