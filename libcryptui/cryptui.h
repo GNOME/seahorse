3ec1d9abb9d8cc5b6feac25a8736efd7949c2281
Adam Schreiber <sadam@gnome.org>
2009-05-13 13:23:28 -0400

    Document enums

--- libcryptui/cryptui.h
+++ libcryptui/cryptui.h
@@ -32,7 +32,16 @@ G_BEGIN_DECLS
 
 #include "cryptui-keyset.h"
 
-/* 
+/**
+ * CryptUIEncType:
+ * @CRYPTUI_ENCTYPE_NONE: the key is none of the below types
+ * @CRYPTUI_ENCTYPE_SYMMETRIC: the key's type is symmetric
+ * @CRYPTUI_ENCTYPE_PUBLIC: the key's type is public
+ * @CRYPTUI_ENCTYPE_PRIVATE: the key's type is private
+ * @_CRYPTUI_ENCTYPE_MAXVALUE: for internal use only
+ *
+ * Used in key properties to indicate what kind of key it is
+ *
  * Never ever change a values already present. These values are used
  * across applications, DBus etc... 
  */
@@ -46,9 +55,22 @@ typedef enum {
     _CRYPTUI_ENCTYPE_MAXVALUE
 } CryptUIEncType;
 
-/* 
+/**
+ * CryptUIKeyFlags:
+ * @CRYPTUI_FLAG_IS_VALID: whether the key is valid
+ * @CRYPTUI_FLAG_CAN_ENCRYPT: whether the key can encrypt
+ * @CRYPTUI_FLAG_CAN_SIGN: whether the key can sign
+ * @CRYPTUI_FLAG_EXPIRED: whether the key is expired
+ * @CRYPTUI_FLAG_REVOKED: whether the key is revoked
+ * @CRYPTUI_FLAG_DISABLED: whether the key is disabled
+ * @CRYPTUI_FLAG_TRUSTED: whether the key is trusted
+ * @CRYPTUI_FLAG_EXPORTABLE: whether the key is exportable
+ *
+ * Used in key properties to indicate a key's status.  These values are
+ * logically OR'd together.
+ *
  * Never ever change a values already present. These values are used
- * across applications, DBus etc... 
+ * across applications, DBus etc...
  */
 typedef enum {
     CRYPTUI_FLAG_IS_VALID =    0x00000001,
@@ -61,19 +83,39 @@ typedef enum {
     CRYPTUI_FLAG_EXPORTABLE =  0x00100000
 } CryptUIKeyFlags;
 
-/* 
+/**
+ * CryptUILocation:
+ * @CRYPTUI_LOC_INVALID: An invalid key
+ * @CRYPTUI_LOC_MISSING: A key we don't know anything about
+ * @CRYPTUI_LOC_SEARCHING: A key we're searching for but haven't found yet
+ * @CRYPTUI_LOC_REMOTE: A key that we've found is present remotely
+ * @CRYPTUI_LOC_LOCAL: A key on the local machine
+ *
+ * Indicates where a key is located.
+ *
  * Never ever change a values already present. These values are used
- * across applications, DBus etc... 
+ * across applications, DBus etc...
  */
 typedef enum {
-    CRYPTUI_LOC_INVALID =        0,    /* An invalid key */
-    CRYPTUI_LOC_MISSING =       10,    /* A key we don't know anything about */
-    CRYPTUI_LOC_SEARCHING =     20,    /* A key we're searching for but haven't found yet */
-    CRYPTUI_LOC_REMOTE =        50,    /* A key that we've found is present remotely */
-    CRYPTUI_LOC_LOCAL =        100,    /* A key on the local machine */
+    CRYPTUI_LOC_INVALID =        0,
+    CRYPTUI_LOC_MISSING =       10,
+    CRYPTUI_LOC_SEARCHING =     20,
+    CRYPTUI_LOC_REMOTE =        50,
+    CRYPTUI_LOC_LOCAL =        100,
 } CryptUILocation;
 
-/* 
+/**
+ * CryptUIValidity:
+ * @CRYPTUI_VALIDITY_REVOKED: the key is revoked
+ * @CRYPTUI_VALIDITY_DISABLED: the key is disabled
+ * @CRYPTUI_VALIDITY_NEVER: the key has never been valid
+ * @CRYPTUI_VALIDITY_UNKNOWN: the key's validity is unknown
+ * @CRYPTUI_VALIDITY_MARGINAL: the key's validity is marginal
+ * @CRYPTUI_VALIDITY_FULL: the key's validity is full
+ * @CRYPTUI_VALIDITY_ULTIMATE: the key's validity is ultimate
+ *
+ * What the validity of the key is.
+ *
  * Never ever change a values already present. These values are used
  * across applications, DBus etc... 
  */
@@ -87,15 +129,15 @@ typedef enum {
     CRYPTUI_VALIDITY_ULTIMATE =  10
 } CryptUIValidity;
 
-/* 
+/*
  * Key Properties:
- * 
+ *
  * display-name: G_TYPE_STRING
- * display-id:   G_TYPE_STRING 
+ * display-id:   G_TYPE_STRING
  * enc-type:     G_TYPE_UINT (CryptUIEncType)
- * 
- * 
- * TODO: Flesh this list out 
+ *
+ *
+ * TODO: Flesh this list out
  */
 
 gchar*              cryptui_key_get_base (const gchar *key);
