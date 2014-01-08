3c2892f061e83c78ea34507034527606815f2279
Stef Walter <stefw@gnome.org>
2014-01-08 12:36:28 +0100

    COPYING: Update to new FSF address and refresh contents
    Refresh the GPL, LGPL and GFDL licenses to those containing
    the new FSF address in the preamble.
    
    In addition simplify all the code license comment headers,
    to not include addresses or such information.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=721549

--- ssh/seahorse-ssh-dialogs.h
+++ ssh/seahorse-ssh-dialogs.h
@@ -1,3 +1,18 @@
+/*
+ * Seahorse
+ *
+ * Copyright (C) 2008 Stefan Walter
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2 of the licence or (at
+ * your option) any later version.
+ *
+ * See the included COPYING file for more information.
+ *
+ * Author: Stef Walter <stefw@collabora.co.uk>
+ */
+
 #ifndef SEAHORSESSHDIALOGS_H_
 #define SEAHORSESSHDIALOGS_H_
 
