4f9f0af92f0b448ed2e093b34aa27e7205fcbf9d
Phil Wyett <philwyett.rebellion@gmail.com>
2017-01-22 19:31:00 +0000

    'autogen.sh' depreciations and warnings
    Fix autogen.sh deperecations and warnings
    
    https://bugzilla.gnome.org/show_bug.cgi?id=777617
    
    Signed-off-by: Stef Walter <stefw@redhat.com>
     * Removed tons of whitespace churn. If desired these should
       be a separate commit

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,6 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="seahorse"
-
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level directory"
@@ -17,4 +15,4 @@ which gnome-autogen.sh || {
     exit 1
 }
 
-REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.9 . gnome-autogen.sh
